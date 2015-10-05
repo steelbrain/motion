2c5e730b051ed8ee482629296098d37d2c908964
natew <natewienert@gmail.com>
2015-10-04 18:59:55 -0700

    start version select

---
+++ apps/tools/modal.js
@@ -0,0 +1,49 @@
+view Modal {
+  <modal>
+    <close>X</close>
+    <title if={^title}>{^title}</title>
+    <message>{^children}</message>
+  </modal>
+
+  $ = {
+    position: 'absolute',
+    top: ^open ? 20 : -100,
+    right: 20,
+    padding: [10, 10],
+    background: '#f9f9f9',
+    border: '1px solid #ddd',
+    boxShadow: '0 0 8px rgba(0,0,0,0.05)',
+    borderRadius: 4,
+    fontSize: 15,
+    transition: 'all ease-in 200ms',
+    textAlign: 'center',
+    pointerEvents: 'auto'
+  }
+
+  $title = {
+    color: '#C43D2D',
+    fontWeight: 'bold',
+    margin: [-4, 20, 0]
+  }
+
+  $message = {
+    fontFamily: "Helvetica Nueue, Helvetica, Arial, sans-serif",
+  }
+
+  $close = {
+    position: 'absolute',
+    top: 3,
+    right: 3,
+    fontWeight: 'bold',
+    fontFamily: 'Helvetica',
+    fontSize: 10,
+    background: '#fff',
+    color: '#333',
+    borderRadius: 20,
+    width: 16,
+    height: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer'
+  }
+}
---
+++ apps/tools/npm/installer.js
@@ -0,0 +1,104 @@
+const tools = window._DT
+
+view Installer {
+  let versions = ['1.0', '2.0', '3.0']
+  let name = ''
+  let error = 'Could not find package something or other'
+  let state = 4
+
+  tools.on('package:install', () => {
+    state = 1
+    name = tools.data.name
+  })
+
+  tools.on('package:installed', () => {
+    state = 2
+    setTimeout(() => state = 0, 2000)
+  })
+
+  tools.on('package:error', () => {
+    state = 3
+    error = tools.data.error
+  })
+
+  tools.on('package:select', () => {
+    state = 4
+    versions = tools.data.versions
+  })
+
+  const selectVersion = version => {
+    tools.data.version = version
+    tools.emit('package:select')
+    state = 1
+  }
+  const closeModal = () => state = 0
+  const title = state => {
+    switch(state) {
+      case 1: return `Installing`; break
+      case 2: return `Installed!`; break
+      case 3: return `Error`; break
+      case 4: return `Select version`; break
+    }
+  }
+  const body = state => {
+    switch(state) {
+      case 1: return null; break
+      case 2: return null; break
+      case 3: return error; break
+      case 4: return <Versions
+        versions={versions}
+        onSelect={selectVersion}
+      />; break
+    }
+  }
+
+  <Modal
+    open={state > 0}
+    onClose={closeModal}
+    title={title(state)}>
+    <InstallerLoading if={false && state < 2} />
+    {body(state)}
+  </Modal>
+}
+
+view InstallerLoading {
+  <svg viewBox="0 14 32 18" width="32" height="4" fill="#f20" preserveAspectRatio="none">
+    <path class="first" opacity="0.8" transform="translate(0 0)" d="M2 14 V18 H6 V14z">
+      <animateTransform attributeName="transform" type="translate" values="0 0; 24 0; 0 0" dur="2s" begin="0" repeatCount="indefinite" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+    </path>
+    <path class="second" opacity="0.5" transform="translate(0 0)" d="M0 14 V18 H8 V14z">
+      <animateTransform attributeName="transform" type="translate" values="0 0; 24 0; 0 0" dur="2s" begin="0.1s" repeatCount="indefinite" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+    </path>
+    <path class="third" opacity="0.25" transform="translate(0 0)" d="M0 14 V18 H8 V14z">
+      <animateTransform attributeName="transform" type="translate" values="0 0; 24 0; 0 0" dur="2s" begin="0.2s" repeatCount="indefinite" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+    </path>
+  </svg>
+
+  $svg = {
+    position: 'absolute',
+    bottom: 0,
+    right: '50%',
+    marginRight: -50,
+    width: 100,
+    height: 17,
+    marginBottom: -15
+  }
+}
+
+view Versions {
+  <version repeat={^versions} onClick={() => ^onSelect(_)}>
+    {_}
+  </version>
+
+  $version = {
+    textAlign: 'left',
+    padding: [2, 4],
+    borderRadius: 2,
+
+    ':hover': {
+      background: '#fff',
+      fontWeight: 'bold',
+      cursor: 'pointer'
+    }
+  }
+}
