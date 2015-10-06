a7bd769d0246eb81616138e1feba95a55adec358
natew <natewienert@gmail.com>
2015-10-06 16:41:14 -0700

    try new release

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,16 +5,39 @@ if [ $# -eq 0 ]; then
   exit 0
 fi
 
-if [ $1 = "tools" ]; then
-  cd apps/tools/.flint
-	npm version patch
-	npm publish
-  cd ../..
-else
+release_package() {
   cd packages/$1
   npm version patch
   npm publish
   cd ../..
+}
+
+release_tools() {
+  cd apps/$1/.flint
+	npm version patch
+	npm publish
+  cd ../..
+}
+
+release_all() {
+  for pkg in packages/*; do
+    [ -d "${pkg}" ] || continue # if not a directory, skip
+    release_package pkg
+  done
+  release_tools
+}
+
+commit_update() {
+  git commit -am "npm publish"
+
+}
+
+if [ $1 = "all" ]; then
+  relase_all
+elif [ $1 = "tools" ]; then
+  relase_tools
+else
+  release_package $1
 fi
 
 # git commit -am 'vbump'
