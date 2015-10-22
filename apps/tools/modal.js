a3442934e614260e1481b5bed3ee3cf125f8950a
natew <natewienert@gmail.com>
2015-10-21 19:34:37 -0700

    new style error message window

--- apps/tools/modal.js
+++ apps/tools/modal.js
@@ -1,27 +1,40 @@
 view Modal {
+  let message
+
+  on('props', setMessage)
+
+  function setMessage() {
+    message = ^children || message
+  }
+
   <modal>
-    <close onClick={^onClose}></close>
+    <close onClick={^onClose}>X</close>
     <title if={^title}>{^title}</title>
-    <message>{^children}</message>
+    <message>{message}</message>
   </modal>
 
   $ = {
-    position: 'absolute',
-    top: ^open ? 20 : -100,
-    left: 20,
+    position: 'fixed',
+    top: ^open ? 0 : -140,
+    left: 0,
+    minWidth: 80,
+    minHeight: 40,
     padding: [10, 10],
+    margin: 20,
     background: '#fff',
-    boxShadow: '0 0 18px rgba(0,0,0,0.1)',
-    borderRadius: 4,
+    boxShadow: '0 0 20px rgba(0,0,0,0.03)',
+    border: '1px solid #dfdfdf',
     fontSize: 15,
     transition: 'all ease-in 200ms',
     textAlign: 'center',
+    borderRadius: 2,
     pointerEvents: 'auto'
   }
 
   $title = {
-    color: '#C43D2D',
-    fontWeight: 'bold',
+    color: '#222',
+    fontWeight: 500,
+    fontSize: 16,
     margin: [0, 20]
   }
 
@@ -31,14 +44,24 @@ view Modal {
 
   $close = {
     position: 'absolute',
-    top: 3,
-    right: 3,
-    background: '#F5DFDD',
-    borderRadius: 20,
-    width: 16,
-    height: 16,
+    top: 0,
+    left: 0,
+    background: '#eee',
+    color: 'red',
+    fontSize: 11,
+    fontWeight: 100,
+    width: 18,
+    height: 18,
     alignItems: 'center',
     justifyContent: 'center',
-    cursor: 'pointer'
+    cursor: 'pointer',
+    borderRadius: 7,
+    border: '3px solid #fff',
+    opacity: 0.2,
+    transition: 'all ease-in 200ms',
+
+    ':hover': {
+      opacity: 1
+    }
   }
 }
