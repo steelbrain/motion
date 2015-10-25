b11f3fd718c9c89cf7bf3c89f472a21ba2bef5bd
natew <natewienert@gmail.com>
2015-10-25 12:35:49 -0700

    checks for invalid values in repeat

--- packages/flint.js/src/lib/range.js
+++ packages/flint.js/src/lib/range.js
@@ -4,4 +4,6 @@ export default function(val) {
   if (typeof val == 'number')
     return Array.apply(null, Array(val))
       .map((_, i) => i + 1)
+  else
+    throw "Must pass an Array or Number to a repeat, you passed " + (typeof val)
 }
