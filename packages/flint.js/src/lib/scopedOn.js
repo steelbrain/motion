27ba08c0b7b123b05000ed591122cd0be78326bb
natew <natewienert@gmail.com>
2015-11-02 17:55:44 -0800

    default view events are available on the object now, on.mount, on.render, etc

--- packages/flint.js/src/lib/scopedOn.js
+++ packages/flint.js/src/lib/scopedOn.js
@@ -1,9 +1,24 @@
 export default function scopedOn(parentScope) {
-  return function(scope, name, cb) {
+  function _on(scope, name, cb) {
     // check if they defined their own scope
+    console.log(name)
     if (name && typeof name == 'string')
       return on(scope, name, cb)
     else
       return on(parentScope, scope, name)
   }
+
+  // view defaults
+  const viewEvent = boundEvent.bind(null, _on, parentScope)
+
+  _on.mount =  viewEvent('mount')
+  _on.unmount = viewEvent('unmount')
+  _on.change = viewEvent('change')
+  _on.render = viewEvent('render')
+
+  return _on
+}
+
+function boundEvent(viewOn, scope, name) {
+  return (...args) => viewOn(scope, name, ...args)
 }
