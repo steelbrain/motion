04c1bb15e8fab3855c9c3a33aae5d46610a34671
natew <natewienert@gmail.com>
2015-11-09 01:00:33 -0800

    fix entries

--- packages/cli/src/bin/flint.js
+++ packages/cli/src/bin/flint.js
@@ -7,10 +7,13 @@ var flintIndex = getFlintIndex()
 
  // find where index of flint is
 function getFlintIndex() {
-  for (let [index, arg] of process.argv.entries()) {
+  let index = 0
+  for (let arg of process.argv) {
     if (arg.indexOf('flint') > 0)
       return index
-    }
+
+    index++
+  }  
 }
 
 // make sure flags are still passed to `flint run`
