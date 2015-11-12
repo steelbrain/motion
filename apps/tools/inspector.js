cb903e7f213d3f98a3a7094bb24ca1ab1688e653
natew <natewienert@gmail.com>
2015-11-11 19:34:42 -0800

    fix things with inspector

--- apps/tools/inspector.js
+++ apps/tools/inspector.js
@@ -19,34 +19,34 @@ view Inspector {
   let views = []
   let clickOff
   let keys = {}
-  let lastTarget = null 
+  let lastTarget = null
 
   function inspect(path) {
-    if (!path || path === temp) return 
+    if (!path || path === temp) return
     temp = path
     view.update()
   }
-  
+
   function mouseMove({ target }) {
     lastTarget = target
     if (hudActive) inspect(findPath(lastTarget))
   }
-  
+
   function removeTemp() {
     temp = null
     view.update()
   }
-  
+
   function close(path, e) {
     if (e) { e.stopPropagation() }
-    
+
     views = views.filter(_id => _id != path)
     setTimeout(view.update, 100)
   }
 
   function glue(e) {
     const path = findPath(e.target)
-    
+
     /* toggle whether views has path */
     if (views.indexOf(path) > -1) {
       close(path)
@@ -60,7 +60,7 @@ view Inspector {
   function showInspect() {
     inspect(findPath(lastTarget))
     hudActive = true
-    clickOff = on(window, 'click', glue)
+    clickOff = on.click(window, glue)
   }
 
   function hideInspect() {
@@ -68,14 +68,14 @@ view Inspector {
     clickOff()
     removeTemp()
   }
-  
+
   function write(path, data) {
     console.log('writing', path, data)
     const name  = data[1][0]
     const current = _Flint.getCache[path][name]
     let value = data[1][1]
     console.log('current is', current)
-    
+
     if (typeof current == 'number') {
       value = +value
     }
@@ -88,10 +88,10 @@ view Inspector {
   }
 
   const isAlt = cb => e => e.keyIdentifier === 'Alt' && cb()
-  let hoverOff = on(window, 'mousemove', mouseMove)
-  
-  on(window, 'keydown', isAlt(showInspect))
-  on(window, 'keyup', isAlt(hideInspect))
+  let hoverOff = on.mousemove(window, mouseMove)
+
+  on.keydown(window, isAlt(showInspect))
+  on.keyup(window, isAlt(hideInspect))
 
   <views>
     <Inspector.View
