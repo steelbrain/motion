a26fedda200097d3096cef7204831b63b9104826
natew <natewienert@gmail.com>
2015-11-13 10:46:11 -0800

    await first install for build

--- packages/runner/src/builder/build.js
+++ packages/runner/src/builder/build.js
@@ -3,23 +3,32 @@ import gulp from '../gulp'
 import bundler from '../bundler'
 import keys from '../keys'
 import copy from './copy'
+import opts from '../opts'
 import makeTemplate from './makeTemplate'
-import log from '../lib/log'
+import { log, handleError } from '../lib/fns'
 
 export default async function build() {
-  log('Building extras, template...')
-  makeTemplate()
+  try {
+    if (!opts.get('hasRunInitialInstall'))
+      setTimeout(build, 50)
 
-  log('Building extras, bundler...')
-  await bundler.install()
-  await bundler.internals()
+    log('Building extras, template...')
+    makeTemplate()
 
-  log('Building extras, copy...')
-  await *[
-    copy.flint(),
-    copy.react(),
-    copy.app()
-  ]
+    log('Building extras, bundler...')
+    await bundler.install()
+    await bundler.internals()
 
-  console.log("\nDone! ⇢  .flint/build\n".green.bold)
+    log('Building extras, copy...')
+    await *[
+      copy.flint(),
+      copy.react(),
+      copy.app()
+    ]
+
+    console.log("\nDone! ⇢  .flint/build\n".green.bold)
+  }
+  catch(e) {
+    handleError(e)
+  }
 }
