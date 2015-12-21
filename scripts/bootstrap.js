5f0da92e5b93ec139d7687f61143de2d42ec01d6
natew <natewienert@gmail.com>
2015-12-21 13:22:22 -0800

    improvements to bootstrap

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -10,6 +10,7 @@ var packages = [];
 
 // order important so they are linkable to each other
 var packageNames = ['transform', 'nice-styles', 'flint.js', 'flint-runner', 'cli'];
+var globalFolder = exec('npm root -g').output.replace("\n", '')
 
 packageNames.forEach(function (loc) {
   var name = path.basename(loc);
@@ -24,47 +25,44 @@ packageNames.forEach(function (loc) {
     pkg: pkg,
     name: pkg.name,
     global: pkg.global,
-    links: pkg.dependencies && Object.keys(pkg.dependencies)
-      .filter(function(package) {
-        return package.indexOf('flint-') === 0
-      }) || []
+    deps: pkg.dependencies && Object.keys(pkg.dependencies) || []
   });
 });
 
 // create links
 packages.forEach(function (pkg) {
-  console.log(pkg.name)
+  console.log('---', pkg.name, '---')
 
   var pkgFolder = "packages/" + pkg.folder
   var nodeModulesLoc = pkgFolder + "/node_modules";
 
   // prune
   cd(pkgFolder)
-  exec('npm prune')
+  console.log('pruning...')
+  exec('npm prune --loglevel=error')
   cd('../..')
 
   mkdir("-p", nodeModulesLoc);
 
-  // link them out
-  packages.forEach(function (sub) {
-    if (!pkg.pkg.dependencies || !pkg.pkg.dependencies[sub.name]) return;
-
-    if (!fs.existsSync(nodeModulesLoc + "/" + sub.name)) {
-      console.log("Linking", "packages/" + sub.folder, "to", nodeModulesLoc + "/" + sub.name);
-      ln("-s", "packages/" + sub.folder, nodeModulesLoc + "/" + sub.name);
-    }
-  });
-
   cd("packages/" + pkg.folder)
+  console.log('installing...')
   exec("npm install --loglevel=error")
 
   // link them in
-  pkg.links.forEach(function(link) {
-    console.log('LINKING IN ', link)
-    exec("npm link " + link)
+  var links = pkg.deps.filter(function(dep) {
+    // if linked into globals, lets link it in
+    var depGlobalPath = path.join(globalFolder, dep).toString()
+    if (dep.indexOf('flint-') > -1 && test('-e', depGlobalPath))
+      return true
   })
 
-  exec("npm link")
+  console.log('linking in other flint packages...')
+  links.forEach(function(link) {
+    console.log(link)
+    exec("npm link " + link + " --loglevel=error")
+  })
+
+  exec("npm link --loglevel=error")
 
   cd("../..");
 });
