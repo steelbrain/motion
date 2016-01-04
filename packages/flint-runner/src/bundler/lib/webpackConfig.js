fa805224f991dc4fb6b1ba3d77d381d6158d83af
natew <natewienert@gmail.com>
2016-01-03 22:36:00 -0800

    adding sass/babel initial support

--- packages/flint-runner/src/bundler/lib/webpackConfig.js
+++ packages/flint-runner/src/bundler/lib/webpackConfig.js
@@ -3,15 +3,11 @@ import path from 'path'
 import opts from '../../opts'
 import cache from '../../cache'
 
-console.log('dirname', path.relative('/', __dirname))
-
-let context = path.join(__dirname, '..', '..', '..', '..')
-let modulesDirectories = path.join(context, 'node_modules')
-
-console.log('context', path.relative('/', context))
+let runnerRoot = path.join(__dirname, '..')
+let modulesDirectories = path.join(runnerRoot, 'node_modules')
 
 export default (config = {}) => deepmerge({
-  context,
+  context: runnerRoot,
   externals: {
     react: 'React',
     'react-dom': 'ReactDOM',
@@ -26,27 +22,27 @@ export default (config = {}) => deepmerge({
   resolveLoader: { root: modulesDirectories },
   resolve: {
     root: opts.get('flintDir'),
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx', '.scss']
   },
   module: {
     loaders: [
       { test: /\.css$/, loaders: ['style', 'css'] },
       {
         test: /\.(png|jpg|gif)$/,
-        loader: 'url-loader?limit=8192&name=[name]-[hash].[ext]'
+        loader: 'url?limit=8192&name=[name]-[hash].[ext]'
       },
       {
         test: /\.scss$/,
         // Query parameters are passed to node-sass
         loader: 'style!css!resolve-url!sass?sourceMap&outputStyle=expanded&' +
-            'includePaths[]=' + (path.resolve(context, './node_modules'))
+            'includePaths[]=' + (path.resolve(runnerRoot, './node_modules'))
       },
       {
         test: /\.(ttf|eot|woff|svg)$/,
         loader: 'file?name=[name]-[hash].[ext]'
       },
       { test: /\.json$/, loader: 'json' },
-      { test: /\.jsx?$/, loader: 'babel' }
+      { test: /\.jsx$/, loader: 'babel' }
     ]
   }
 }, config)
