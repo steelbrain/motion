5375a4d237970ce5054d1b925192b7aa9a488564
natew <natewienert@gmail.com>
2016-01-21 18:59:44 -0800

    prefix

--- packages/flint.js/client/lib/radium.js
+++ packages/flint.js/client/lib/radium.js
@@ -4,7 +4,8 @@ export default function ConfiguredRadium(component) {
   return Radium({
     plugins: [
       Radium.Plugins.resolveMediaQueries,
-      Radium.Plugins.resolveInteractionStyles
+      Radium.Plugins.resolveInteractionStyles,
+      Radium.Plugins.prefix,
     ],
   })(component)
 }
