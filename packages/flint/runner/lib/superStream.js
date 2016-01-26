7d8fdfc52887484047b693222c12c07d42c38ad6
natew <natewienert@gmail.com>
2016-01-26 15:03:29 -0800

    start refactor stream lock system to by async

--- packages/flint/runner/lib/superStream.js
+++ packages/flint/runner/lib/superStream.js
@@ -6,105 +6,89 @@ import cache from '../cache'
 import bridge from '../bridge'
 import { _, path, log, readFile, handleError, vinyl } from '../lib/fns'
 
+const isFileType = (_path, ext) => path.extname(_path) == `.${ext}`
 const debug = log.bind(null, { name: 'stream', icon: '🚀' })
 
-let basePath, flintPath
-let stream = new Readable({ objectMode: true })
-stream._read = function(n) {}
-
-let internalTimeout
-let fileLoading = {}
-let scriptWaiting = {}
-
-function isFileType(_path, ext) {
-  return path.extname(_path) == `.${ext}`
-}
-
-function fileSend({ path, contents }) {
-  debug(path)
-
-  // check if file actually in flint project
-  if (!path || path.indexOf(basePath) !== 0 || path.indexOf(flintPath) === 0 || !isFileType(path, 'js')) {
-    debug('  file no JS, not in path, or is in .flint dir', basePath, flintPath, path)
-    return
-  }
-
-  // write to stream
-  const rPath = nodepath.relative(basePath, path)
-  const file = new File(vinyl(basePath, path, new Buffer(contents)))
-
-  debug('SIN', rPath)
-
-  function pushStream() {
-    debug('SOUT', rPath)
-    stream.push(file)
-  }
-
-  let attempts = 0
-
-  // waits for file load before pushing next
-  function checkPushStream(loop = false) {
-    if (!loop) {
-      // ensures it only runs once after loaded
-      if (scriptWaiting[rPath]) return
-      scriptWaiting[rPath] = true
-    }
-
-    debug('RELOAD', fileLoading[rPath])
-    // loop waiting for browser to finish loading
-    if (fileLoading[rPath]) {
-      if (++attempts > 50) {
-        // ceil attempts to avoid locks
-        debug('ATTEMPTS > 50!!')
-        fileLoading[rPath] = false
-        scriptWaiting[rPath] = false
-      }
-      else {
-        // loop
-        setTimeout(() => checkPushStream(true), 20)
-        return
-      }
-    }
-
-    // fileLoading[rPath] = true
-    scriptWaiting[rPath] = false
-    pushStream()
-  }
-
-  // internals debounce
-  if (cache.isInternal(path)) {
-    debug('is exported!')
-    clearTimeout(internalTimeout)
-    internalTimeout = setTimeout(pushStream, 1000)
-  }
-  else {
-    checkPushStream()
-  }
-}
-
-// ignore stream when loading file in browser
-function initScriptWait() {
-  bridge.on('script:load', ({ path }) => {
-    debug('IN', 'browser loading', path)
-    fileLoading[path] = true
-  })
-
-  bridge.on('script:done', ({ path }) => {
-    debug('OUT', 'browser done', path)
-    fileLoading[path] = false
-  })
-}
+let basePath, flintPath, relPath
 
 function init() {
   basePath = opts('appDir')
   flintPath = opts('flintDir')
+  relPath = p => nodepath.relative(basePath, p)
+  watchForBrowserLoading()
+  bridge.on('live:save',
+    _.throttle(fileSend, 22, { leading: true }) // throttle the stream a bit
+  )
+}
 
-  initScriptWait()
+// ignore stream when loading file in browser
+function watchForBrowserLoading() {
+  bridge.on('script:load', ({ path }) => {
+    debug('IN', 'browser loading', path)
+    browserLoading[path] = true
+  })
 
-  // throttle the stream a bit
-  let fileSender = _.throttle(fileSend, 22, { leading: true })
+  bridge.on('script:done', ({ path }) => {
+    debug('IN', 'browser done', path)
+    browserLoading[path] = false
+    loadWaiting(path)
+  })
+}
 
-  bridge.on('live:save', fileSender)
+let stream = new Readable({ objectMode: true })
+stream._read = function(n) {}
+let internalTimeout
+let browserLoading = {}
+let queue = {}
+
+function fileSend({ path, contents }) {
+  // check if file actually in flint project
+  if (!path || path.indexOf(basePath) !== 0 || path.indexOf(flintPath) >= 0 || !isFileType(path, 'js')) {
+    debug('  file not js || not in path || in .flint', path)
+    return
+  }
+
+  // write to stream
+  const relative = relPath(path)
+  const file = new File(vinyl(basePath, path, new Buffer(contents)))
+  debug('SIN', relative)
+
+  // internals debounce // TODO watch for export finish
+  if (cache.isInternal(path)) {
+    debug('is exported!')
+    clearTimeout(internalTimeout)
+    internalTimeout = setTimeout(pushStream, 300)
+    return
+  }
+
+  pushStreamRun(relative, () => {
+    debug('SOUT', relative)
+    queue[relative] = false
+    stream.push(file)
+  })
+}
+
+function pushStreamRun(relative, finish) {
+  // wait for previous load
+  if (browserLoading[relative]) {
+    let checkTimeout
+    queue[relative] = finish
+    // ensure upper limit on wait
+    checkTimeout = setTimeout(() => {
+      if (!queue[relative]) return
+      browserLoading[relative] = false
+      finish()
+    }, 200)
+  }
+  else {
+    finish()
+  }
+}
+
+// load waiting
+function loadWaiting(path) {
+  const queued = queue[relPath(path)]
+  if (queued) queued()
 }
 
 export default { init, stream }
