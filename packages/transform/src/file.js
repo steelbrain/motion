428ce517248c3d3c913b4169ce270b11d580d8d8
natew <natewienert@gmail.com>
2016-01-27 16:18:39 -0800

    comment

--- packages/transform/src/file.js
+++ packages/transform/src/file.js
@@ -795,15 +795,15 @@ export default function createPlugin(options) {
 
         BlockStatement: {
           exit(node) {
-            if (!node.blockTracked && viewBlockHasState) {
-              viewBlockHasState = false
-
-              console.log(node.body)
-
-              return node
-            }
-
-            node.blockTracked = true
+            // if (!node.blockTracked && viewBlockHasState) {
+            //   viewBlockHasState = false
+            //
+            //   console.log(node.body)
+            //
+            //   return node
+            // }
+            //
+            // node.blockTracked = true
           }
         },
 
