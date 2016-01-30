c2b36f832d8185bf0b03aea52916e6e908e7ab50
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-30 09:14:28 -0700

    :new: Detect position type properly

--- packages/flint/runner/autocomplete.js
+++ packages/flint/runner/autocomplete.js
@@ -3,28 +3,50 @@
 import {transformFile, pointWithinRange, getObjectAtPosition} from './helpers'
 import Point from 'atom-text-buffer-point'
 
+export const POSITION_TYPE = {
+  VIEW_TOP: 'VIEW_TOP',
+  VIEW_JSX: 'VIEW_JSX',
+  STYLE: 'STYLE'
+}
+
 export default class Autocomplete {
-  constructor() {
-
-  }
   provideAutocomplete(text, position) {
-    const point = Point.fromObject(position)
-
+    position = Point.fromObject(position)
+    const views = this.scanViews(text)
+    const viewsActive = getObjectAtPosition(views, position)
+    if (viewsActive === null) {
+      // We're at top level in file
+      return []
+    }
+    const viewsPosition = this.getPositionInfo(viewsActive, position)
+    if (viewsPosition === POSITION_TYPE.STYLE) {
+      // css
+    } else if (viewsPosition === POSITION_TYPE.VIEW_JSX) {
+      // jsx tags
+    } else if (viewsPosition === POSITION_TYPE.VIEW_TOP) {
+      // maybe autocomplete $h1?
+    }
+    return []
+  }
+  scanViews(text) {
     let views = {}
     transformFile(text, {
       onMeta: function(meta) {
         views = meta.views
       }
     })
-
-    const activeView = getObjectAtPosition(views, point)
-    console.log(activeView)
-    return []
+    return views
   }
-  activate() {
-
+  getPositionInfo(view, position) {
+    if (getObjectAtPosition(view.els, position)) {
+      return POSITION_TYPE.VIEW_JSX
+    }
+    if (getObjectAtPosition(view.styles, position)) {
+      return POSITION_TYPE.STYLE
+    }
+    return POSITION_TYPE.VIEW_TOP
   }
-  dispose() {
+  getStyleAutocomplete() {
 
   }
 }
