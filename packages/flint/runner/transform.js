19c76b7f2675c73c1b55b4e11ebdbdae26c7bc21
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-30 15:05:43 -0700

    :new: Add transform class

---
+++ packages/flint/runner/transform.js
@@ -0,0 +1,76 @@
+'use babel'
+
+import FlintTransform from 'flint-transform'
+import {CompositeDisposable, Emitter} from 'sb-event-kit'
+import {isProduction} from './helpers'
+import getOption from './opts'
+
+// The purpose of this class is to serve as a proxy to transform package
+// The transform package uses files instead of instances to store state
+// Therefore this file is required to fix an overwrite bug, discovered
+// by autocomplete
+
+export default class Transform {
+  constructor() {
+    this.emitter = new Emitter()
+    this.disposables = []
+    this.transform = null
+  }
+  get(config) {
+    if (this.transform === null) {
+      this.transform = FlintTransform.file({
+        basePath: getOption('dir'),
+        production: isProduction(),
+        selectorPrefix: getOption('config').selectorPrefix || '#_flintapp ',
+        log: () => {
+          this.emitter.emit('log', ...arguments)
+        },
+        writeStyle: (a, b) => {
+          this.emitter.emit('writeStyle', a, b)
+        },
+        onMeta: (state) => {
+          this.emitter.emit('onMeta', state)
+        },
+        onImports: (file, imports) => {
+          this.emitter.emit('onImports', file, imports)
+        },
+        onExports: (file, imports) => {
+          this.emitter.emit('onExports', file, imports)
+        }
+      })
+    }
+
+    const disposable = new CompositeDisposable()
+    if (config.log) {
+      disposable.add(this.emitter.on('log', config.log))
+    }
+    if (config.writeStyle) {
+      disposable.add(this.emitter.on('writeStyle', config.writeStyle))
+    }
+    if (config.onMeta) {
+      disposable.add(this.emitter.on('onMeta', config.onMeta))
+    }
+    if (config.onImports) {
+      disposable.add(this.emitter.on('onImports', config.onImports))
+    }
+    if (config.onExports) {
+      disposable.add(this.emitter.on('onExports', config.onExports))
+    }
+    this.disposables.push(disposable)
+    return this.transform
+  }
+  disposeLast() {
+    const disposable = this.disposables.pop()
+    if (disposable) {
+      disposable.dispose()
+    }
+  }
+  dispose() {
+    this.emitter.dispose()
+    this.disposables.forEach(function(disposable) {
+      disposable.dispose()
+    })
+    this.disposable = null
+    this.transform = null
+  }
+}
