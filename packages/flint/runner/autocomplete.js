9351f1c05dd817c588032758986451aa00668c99
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-30 08:45:27 -0700

    :new: Figure out active view from position

--- packages/flint/runner/autocomplete.js
+++ packages/flint/runner/autocomplete.js
@@ -1,10 +1,24 @@
 'use babel'
 
+import {transformFile, pointWithinRange, getObjectAtPosition} from './helpers'
+import Point from 'atom-text-buffer-point'
+
 export default class Autocomplete {
   constructor() {
 
   }
   provideAutocomplete(text, position) {
+    const point = Point.fromObject(position)
+
+    let views = {}
+    transformFile(text, {
+      onMeta: function(meta) {
+        views = meta.views
+      }
+    })
+
+    const activeView = getObjectAtPosition(views, point)
+    console.log(activeView)
     return []
   }
   activate() {
