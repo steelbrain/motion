357639865056cec8916fcf08e74976b8a9aa41f0
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-30 09:53:27 -0700

    :new: Make getStyleAutocomplete work

--- packages/flint/runner/autocomplete.js
+++ packages/flint/runner/autocomplete.js
@@ -1,13 +1,17 @@
 'use babel'
 
-import {transformFile, pointWithinRange, getObjectAtPosition} from './helpers'
 import Point from 'atom-text-buffer-point'
+import string_score from 'sb-string_score'
+import {transformFile, pointWithinRange, getObjectAtPosition, getRowFromText} from './helpers'
+import Styles from './autocomplete-styles'
 
 export const POSITION_TYPE = {
   VIEW_TOP: 'VIEW_TOP',
   VIEW_JSX: 'VIEW_JSX',
   STYLE: 'STYLE'
 }
+const VALUE_VALIDATION_REGEX = /['"]?\S+['"]?: *['"]?[^,"]*$/
+const PREFIX_REGEX = /['"]?([a-zA-Z0-9]+)$/
 
 export default class Autocomplete {
   provideAutocomplete(text, position) {
@@ -21,6 +25,7 @@ export default class Autocomplete {
     const viewsPosition = this.getPositionInfo(viewsActive, position)
     if (viewsPosition === POSITION_TYPE.STYLE) {
       // css
+      return this.getStyleAutocomplete(text, position)
     } else if (viewsPosition === POSITION_TYPE.VIEW_JSX) {
       // jsx tags
     } else if (viewsPosition === POSITION_TYPE.VIEW_TOP) {
@@ -46,7 +51,31 @@ export default class Autocomplete {
     }
     return POSITION_TYPE.VIEW_TOP
   }
-  getStyleAutocomplete() {
+  getStyleAutocomplete(text, position) {
+    const lineText = getRowFromText(text, position.row).slice(0, position.column)
+    if (VALUE_VALIDATION_REGEX.test(lineText)) {
+      // Ignore value suggestions, 'cause we already provided them with key
+      return []
+    }
+    // Autocomplete key
+    const suggestions = Styles.slice()
+    suggestions.sort(function(a, b) {
+      return b.strength - a.strength
+    })
 
+    let prefix = PREFIX_REGEX.exec(lineText)
+    prefix = prefix ? prefix[1] : ''
+    if (prefix === '') {
+      return suggestions
+    }
+    suggestions.forEach(function(suggestion) {
+      suggestion.matchScore = string_score(suggestion.name, prefix)
+    })
+    suggestions.sort(function(a, b) {
+      return b.matchScore - a.matchScore
+    })
+    return suggestions.filter(function(item) {
+      return item.matchScore !== 0
+    })
   }
 }
