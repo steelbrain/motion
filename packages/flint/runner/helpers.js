e52db47e7dc159d7781b2be79fcb8c6587a81fc0
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-31 06:58:19 -0700

    :minibus: Move all of editor helpers in editor/helpers

--- packages/flint/runner/helpers.js
+++ packages/flint/runner/helpers.js
@@ -1,12 +1,9 @@
 'use babel'
 
 import TransformPlugin from './transform'
-import {transform as babelTransform} from 'flint-babel-core'
 import getOption from './opts'
-import {Parser} from './compiler'
 
-const NEWLINE_REGEX = /\r\n|\n|\r/g
-const transformPlugin = new TransformPlugin()
+export const transformPlugin = new TransformPlugin()
 
 export function isProduction() {
   return getOption('build')
@@ -24,40 +21,4 @@ export function getBabelConfig(config) {
     plugins: [transformPlugin.get(config)],
     extra: { production: isProduction() }
   }
-}
-
-export function transformText(text, {
-  log = null,
-  writeStyle = null,
-  onMeta = null,
-  onImports = null,
-  onExports = null
-}) {
-  let toReturn = ''
-  Parser.pre('unknown', text, function(text) {
-    toReturn = babelTransform(text, getBabelConfig({
-      log, writeStyle, onMeta, onImports, onExports
-    }))
-  })
-  transformPlugin.disposeLast()
-  return toReturn
-}
-
-export function pointWithinRange(point, range) {
-  return point.isGreaterThan(range[0]) && point.isLessThan(range[1])
-}
-
-export function getObjectAtPosition(objects, position) {
-  for (const key in objects) {
-    const value = objects[key]
-    if (pointWithinRange(position, value.location)) {
-      return value
-    }
-  }
-  return null
-}
-
-export function getRowFromText(text, row) {
-  const rowText = text.split(NEWLINE_REGEX)[row]
-  return rowText || ''
 }
