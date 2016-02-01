bb8f98a0032d05ad0ff34d052291bd60d5e7e5b0
steelbrain <aneesiqbalbhatti@gmail.com>
2016-02-01 10:37:12 -0700

    :art: Import promisify from sb-promisify

--- packages/flint/runner/lib/fns.js
+++ packages/flint/runner/lib/fns.js
@@ -1,3 +1,4 @@
+import promisify from 'sb-promisify'
 import _ from 'lodash'
 import _glob from 'globby'
 import readdirp from 'readdirp'
@@ -53,31 +54,6 @@ async function globCopy(pattern, dest, opts = {}) {
 
 function sanitize(str) {
   return str.replace(/[^a-zA-Z]/, '')
-}
-
-function promisify(callback){
-  return function promisified(){
-    const parameters = arguments
-    const parametersLength = arguments.length + 1
-    return new Promise((resolve, reject) => {
-      Array.prototype.push.call(parameters, function(error, data) {
-        if (error) {
-          reject(error)
-        } else resolve(data)
-      })
-      if (parametersLength === 1) {
-        callback.call(this, parameters[0])
-      } else if (parametersLength === 2) {
-        callback.call(this, parameters[0], parameters[1])
-      } else if (parametersLength === 3) {
-        callback.call(this, parameters[0], parameters[1], parameters[2])
-      } else if (parametersLength === 4) {
-        callback.call(this, parameters[0], parameters[1], parameters[2], parameters[3])
-      } else {
-        callback.apply(this, parameters)
-      }
-    })
-  }
 }
 
 function vinyl(basePath, path, contents) {
