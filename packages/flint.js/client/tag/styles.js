8692a0e0132516e3a9dfa131c8e39c82521d7fd7
natew <natewienert@gmail.com>
2016-02-02 20:07:12 -0800

    move timer for now

--- packages/flint.js/client/tag/styles.js
+++ packages/flint.js/client/tag/styles.js
@@ -39,6 +39,9 @@ export default function elementStyles(el, view, props) {
     props.className = props.className ? `${props.className} ${name}` : name
   }
 
+  if (view.name === 'Page.Sidebar')
+    console.log(el.name, deservesRootStyles)
+
   if (deservesRootStyles) {
     if (view.props.__flint) {
       const parentName = view.props.__flint.parentName
