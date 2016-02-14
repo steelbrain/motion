38341292e11c201c88b0ee1c2cd87e1a8fe73930
natew <natewienert@gmail.com>
2016-02-13 17:38:03 -0800

    add message on migration

--- packages/motion/src/cli/lib/checkForApp.js
+++ packages/motion/src/cli/lib/checkForApp.js
@@ -2,10 +2,25 @@ import path from 'path'
 import { statSync } from 'fs'
 
 try {
-  statSync(path.join(process.cwd(), '/.motion'))
+  statSync(path.join(process.cwd(), '.motion'))
 }
 catch(e) {
-  console.log("\nNot in motion app!\n".red)
-  process.argv.push('--help')
+  console.log("\n not in a motion app directory\n")
+
+  try {
+    statSync(path.join(process.cwd(), '.flint'))
+    console.log(`
+    Steps to migrate from Flint to Motion are here:
+      https://github.com/motion/motion/tree/master/MIGRATE.md
+
+    Short version:
+      mv .flint .motion
+      mv .motion/flint.json .motion/config.js
+
+      replace "Flint" with "Motion" in your app code
+    `)
+  }
+  catch(e) {}
+
   process.exit()
 }
