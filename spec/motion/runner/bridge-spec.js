c97e66439adc096aa5b7d7a943f87e783d1eee9d
steelbrain <de.steelbrain@gmail.com>
2016-02-16 06:29:37 +0000

    :new: Add specs for bridge::handleConnection

--- spec/motion/runner/bridge-spec.js
+++ spec/motion/runner/bridge-spec.js
@@ -1,5 +1,6 @@
 'use babel'
 
+import {EventEmitter} from 'events'
 import {it, wait} from '../../helpers'
 import {Bridge} from '../../../packages/motion/lib/runner/bridge.js'
 
@@ -7,11 +8,19 @@ describe('Bridge', function() {
   let bridge
 
   beforeEach(function() {
-    if (bridge) {
-      bridge.dispose()
-    }
     bridge = new Bridge()
   })
+  afterEach(function() {
+    bridge.dispose()
+  })
+
+  function createDummyConnection() {
+    const connection = new EventEmitter()
+    spyOn(connection, 'on').andCallThrough()
+    spyOn(connection, 'emit').andCallThrough()
+    connection.send = jasmine.createSpy('connection::send')
+    return connection
+  }
 
   describe('::activate', function() {
     it('resolves when the server has started listening', async function() {
@@ -21,4 +30,22 @@ describe('Bridge', function() {
     })
   })
 
+  describe('::handleConnection', function() {
+    it('sends initialization messages', function() {
+      const connection = createDummyConnection()
+      bridge.handleConnection(connection)
+      expect(connection.on).toHaveBeenCalled()
+      expect(connection.send).toHaveBeenCalled()
+      expect(connection.send.calls.length).toBe(2)
+      expect(JSON.parse(connection.send.calls[0].args[0])._type).toBe('motion:baseDir')
+      expect(JSON.parse(connection.send.calls[1].args[0])._type).toBe('motion:opts')
+    })
+    it('sends cached messages', function() {
+      const connection = createDummyConnection()
+      bridge.broadcast('editor:error', {}, 'editor')
+      bridge.handleConnection(connection)
+      expect(connection.send.calls.length).toBe(3)
+      expect(JSON.parse(connection.send.calls[2].args[0])._type).toBe('editor:error')
+    })
+  })
 })
