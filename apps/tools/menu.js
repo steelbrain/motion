a5e0cb38ba7231a261c59ac2b7ef9d7e6db7ad8b
Nick <you@example.com>
2016-02-20 18:56:54 -0800

    fixed jump to editor

--- apps/tools/menu.js
+++ apps/tools/menu.js
@@ -51,11 +51,12 @@ view Menu {
   function focusElement(el) {
     return function() {
       // slicing because h11 -> h1
+      const view = el.view.replace(/\-/g, '.')
       toEditor({
         type: 'focus:element',
         key: el.key.slice(0, -1),
-        view: el.view,
-        filePath: _Motion.views[el.view].file
+        view,
+        filePath: _Motion.views[view].file
       })
     }
   }
