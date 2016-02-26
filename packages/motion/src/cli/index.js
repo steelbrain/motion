c3776e5a4d9451d9c98d5df58774a0f1cca83860
steelbrain <de.steelbrain@gmail.com>
2016-02-26 15:53:28 -0700

    :new: Satisfiy commander with a command specific process.argv

--- packages/motion/src/cli/index.js
+++ packages/motion/src/cli/index.js
@@ -9,6 +9,9 @@ const validCommands = ['new', 'build', 'update', 'init']
 
 // TODO: Check for updates
 
+// Note: This is a trick to make multiple commander commands work with single executables
+process.argv = process.argv.slice(0, 2).concat(process.argv.slice(3))
+
 if (!command || command === 'run') {
   require('./motion-run')
 } else if (validCommands.indexOf(command) !== -1) {
