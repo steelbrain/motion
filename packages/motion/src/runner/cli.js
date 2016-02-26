7431bd2d46f1da358e646adf76981a60af11d3b1
steelbrain <de.steelbrain@gmail.com>
2016-02-26 13:58:47 -0700

    :new: Add addCommand to CLI class

--- packages/motion/src/runner/cli.js
+++ packages/motion/src/runner/cli.js
@@ -2,7 +2,7 @@
 
 /* @flow */
 
-import {CompositeDisposable, Emitter} from 'sb-event-kit'
+import {CompositeDisposable, Emitter, Disposable} from 'sb-event-kit'
 import vorpal from 'vorpal'
 
 const CLI_DELIMITER = 'motion>'
@@ -25,6 +25,16 @@ export class CLI {
     this.instance.delimiter(CLI_DELIMITER)
     this.instance.show()
     this.active = true
+  }
+  addCommand(name: string, helpText: string, callback: Function) {
+    this.instance.command(name, helpText).action(function(args, keepRunning) {
+      const result = callback(args)
+      if (result) {
+        return result
+      } else {
+        keepRunning()
+      }
+    })
   }
   dispose() {
     if (this.active) {
