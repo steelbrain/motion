7e9720b358441646295a67e88f2939ac9ee079f8
steelbrain <de.steelbrain@gmail.com>
2016-02-27 10:12:27 -0700

    :tada: Use the new CLI class in runner

--- packages/motion/src/runner/index.js
+++ packages/motion/src/runner/index.js
@@ -1,8 +1,18 @@
-import { run, build } from './startup'
+'use strict'
+
+import { CLI } from './cli'
+import { run as runStartup, build } from './startup'
 
 // so we can easily weed out console.logs
 // print = keep
 // console.log = delete
 global.print = console.log.bind(console)
 
-export default { run, build }
+async function run() {
+  const cli = new CLI()
+  cli.activate()
+  return cli
+}
+
+// TODO: This is for backward compatibility only, remove this in the upcoming future
+export default { build, run }
