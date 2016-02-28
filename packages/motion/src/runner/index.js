0e967d1ae22488f5ac5a8b467833dab87ec9561f
steelbrain <de.steelbrain@gmail.com>
2016-02-28 15:03:02 -0700

    :bug: Only show UI on TTYs

--- packages/motion/src/runner/index.js
+++ packages/motion/src/runner/index.js
@@ -16,28 +16,35 @@ const realPath = promisify(FS.realpath)
 global.print = console.log.bind(console)
 
 async function run(options) {
-  const cli = new CLI()
-  cli.activate()
-  // Make printing go to our shiny UI
-  global.print = function() {
-    cli.log(...arguments)
+  const showUI = proc.stdin.isTTY
+  let cli
+
+  if (showUI) {
+    cli = new CLI()
+    cli.activate()
+    // Make printing go to our shiny UI
+    global.print = function() {
+      cli.log(...arguments)
+    }
   }
+
   await runStartup(options, cli)
 
-  // Startup messages
-  cli.log(chalk.green('Server running at') + ' ' + chalk.yellow('http://' + server.url()))
+  if (showUI) {
+    // Startup messages
+    cli.log(chalk.green('Server running at') + ' ' + chalk.yellow('http://' + server.url()))
 
-  cli.addCommand('open', 'Open this project in Browser', async function() {
-    open('http://' + server.url())
-  })
-  cli.addCommand('editor', 'Open this project in Atom', async function() {
-    const directory = await realPath(process.cwd())
-    await exec('atom', [directory])
-  })
-  cli.addCommand('build', 'Build dist files of your motion app', async function() {
-    await builder.build()
-  })
-  return cli
+    cli.addCommand('open', 'Open this project in Browser', async function() {
+      open('http://' + server.url())
+    })
+    cli.addCommand('editor', 'Open this project in Atom', async function() {
+      const directory = await realPath(process.cwd())
+      await exec('atom', [directory])
+    })
+    cli.addCommand('build', 'Build dist files of your motion app', async function() {
+      await builder.build()
+    })
+  }
 }
 
 // TODO: This is for backward compatibility only, remove this in the upcoming future
