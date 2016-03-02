57797f83a74e437802be58091d71510a2de439a2
steelbrain <de.steelbrain@gmail.com>
2016-03-02 13:14:27 -0700

    :art: Make manifestPath search two level up

--- packages/npm/src/helpers.js
+++ packages/npm/src/helpers.js
@@ -26,6 +26,10 @@ export async function manifestPath(name: string, rootDirectory: string): Promise
     manifestPath = Path.normalize(Path.join(rootDirectory, '..', 'node_modules', name, 'package.json'))
   }
   if (!await exists(manifestPath)) {
+    // $PROJECT_PATH/../../node_modules/$NAME/package.json
+    manifestPath = Path.normalize(Path.join(rootDirectory, '..', '..', 'node_modules', name, 'package.json'))
+  }
+  if (!await exists(manifestPath)) {
     throw new Error(`Unable to determine package installation path for ${name}`)
   }
   return manifestPath
