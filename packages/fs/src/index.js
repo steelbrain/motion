4ca81eb3e33a007f18a9aa59051d9bea4a34cd4b
steelbrain <de.steelbrain@gmail.com>
2016-03-07 13:36:27 -0700

    :new: Add writeJSON method in motion-fs

--- packages/fs/src/index.js
+++ packages/fs/src/index.js
@@ -12,6 +12,9 @@ export async function readJSON(filePath: string, encoding: string = 'utf8'): Pro
   const contents = await readFile(filePath)
   return JSON.parse(contents.toString(encoding))
 }
+export async function writeJSON(filePath: string, contents: Object): Promise {
+  await writeFile(filePath, JSON.stringify(contents))
+}
 export function exists(filePath: string): Promise<boolean> {
   return new Promise(function(resolve) {
     FS.access(filePath, FS.R_OK, function(error) {
