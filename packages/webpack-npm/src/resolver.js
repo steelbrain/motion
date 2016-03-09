a497464c27f1ca3935f76ecccf744c4aac296e1e
steelbrain <de.steelbrain@gmail.com>
2016-03-09 12:29:50 -0700

    :new: Simplify resolver

--- packages/webpack-npm/src/resolver.js
+++ packages/webpack-npm/src/resolver.js
@@ -2,60 +2,50 @@
 
 /* @flow */
 
-import resolve from 'resolve'
+import promisify from 'sb-promisify'
 import NPM from 'motion-npm'
-import { getModuleName, getRootDirectory } from './helpers'
+import { getModuleName, getRootDirectory, isBuiltin } from './helpers'
 import type { Installer$Config } from './types'
 
+const resolve = promisify(require('resolve'))
 let installID = 0
 
 export function getResolver(config: Installer$Config, compiler: Object, loader: boolean): Function {
   const locks = new Set()
   const npm = new NPM({rootDirectory: getRootDirectory()})
 
-  return function(result: Object, next: Function) {
+  return async function(result: Object, next: Function): Promise {
+    const id = ++installID
     const moduleName = getModuleName(result, loader)
-    if (locks.has(moduleName)) {
+    let error = null
+
+    if (locks.has(moduleName) || isBuiltin(moduleName) || await npm.isInstalled(moduleName)) {
       next()
       return
     }
     locks.add(moduleName)
-    npm.isInstalled(moduleName).then(function(status) {
-      if (status) {
-        next()
-        return
-      }
-      const id = ++installID
-      if (config.onStarted) {
-        config.onStarted(id, [[moduleName, 'x.x.x']])
-      }
-      return npm.install(moduleName, config.save).then(function() {
-        if (config.onProgress) {
-          config.onProgress(id, moduleName, null)
-        }
-        if (config.onComplete) {
-          config.onComplete(id)
-        }
-        resolve(moduleName, { basedir: result.path }, function(error, path) {
-          if (error) {
-            next()
-          } else {
-            next(null, Object.assign({}, result, {
-              path, resolved: true
-            }))
-          }
-        })
-      }, function(error) {
-        if (config.onProgress) {
-          config.onProgress(id, moduleName, error)
-        }
-        if (config.onComplete) {
-          config.onComplete(id)
-        }
-        next()
-      })
-    }).catch(function(error) {
-      console.error('[motion-webpack-npm] Uncaught error', error)
-    })
+    if (config.onStarted) {
+      config.onStarted(id, [[moduleName, 'x.x.x']])
+    }
+    try {
+      await npm.install(moduleName, config.save)
+    } catch (_) {
+      error = _
+    }
+    if (config.onProgress) {
+      config.onProgress(id, moduleName, error)
+    }
+    if (config.onComplete) {
+      config.onComplete(id)
+    }
+    locks.delete(moduleName)
+    try {
+      const path = await resolve(moduleName, { basedir: result.path })
+      next(null, Object.assign({}, result, {
+        path, resolved: true
+      }))
+    } catch (_) {
+      next()
+    }
   }
 }
