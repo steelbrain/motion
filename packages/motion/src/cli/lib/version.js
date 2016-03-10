974650e3925f2843796bcac28322ace4d1f70de4
steelbrain <de.steelbrain@gmail.com>
2016-03-10 08:54:28 -0700

    :fire: Cleanup main motion package

--- packages/motion/src/cli/lib/version.js
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-import fs from 'fs'
-
-const fpackage = path.join(__dirname, '..', '..', '..', 'package.json')
-const fstr = fs.readFileSync(fpackage, 'utf-8')
-const fjson = JSON.parse(fstr)
-const version = fjson.version
-
-export default version
--- packages/motion/src/runner/lib/exec.js
+++
@@ -1,6 +0,0 @@
-var _exec = require('child_process').exec
-var uid = process.getuid()
-
-export default function exec(cmd, cwd, cb) {
-  _exec(cmd, { uid, cwd }, cb)
-}
