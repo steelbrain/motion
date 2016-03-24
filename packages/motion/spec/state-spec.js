55460b82bcacc45cf3f5fc73fc901397d3bb3cba
steelbrain <de.steelbrain@gmail.com>
2016-03-23 22:44:30 -0700

    :bug: Fix CI build

--- packages/motion/spec/state-spec.js
+++ packages/motion/spec/state-spec.js
@@ -10,13 +10,15 @@ const { it } = require(process.env.SPEC_HELPER_SCRIPT)
 describe('Motion State', function() {
   it('resumes an existing state', async function() {
     const statePath = Path.join(__dirname, 'fixtures', 'state.json')
-    const state = await State.create(statePath)
+    const configPath = Path.join(__dirname, 'fixtures', 'config.json')
+    const state = await State.create(statePath, configPath)
     expect(state.get().running).toBe(true)
   })
 
   it('creates a shallow in memory state if file does not exist', async function() {
     const statePath = Path.join(__dirname, 'fixtures', 'state-non-existent.json')
-    const state = await State.create(statePath)
+    const configPath = Path.join(__dirname, 'fixtures', 'config.json')
+    const state = await State.create(statePath, configPath)
     expect(state.get().running).toBe(false)
   })
 })
