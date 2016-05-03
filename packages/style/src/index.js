b6b672862fa2b4be09ce8d0a60402cb97b0db586
natew <natewienert@gmail.com>
2016-05-03 11:59:09 -0700

    :art: linting and style in motion-style

--- packages/style/src/index.js
+++ packages/style/src/index.js
@@ -10,8 +10,9 @@ export default function Style(ComposedComponent) {
       if (this.style) {
         const styles = Object.assign({}, this.style)
 
-        for (let style in styles)
-          niceStyles(styles[style])
+        for (const style in styles) {
+          if (style) niceStyles(styles[style])
+        }
 
         this.stylesheet = StyleSheet.create(styles)
       }
@@ -24,33 +25,26 @@ export default function Style(ComposedComponent) {
     }
 
     styleAll(children) {
-      if (!children || !Array.isArray(children) && !children.props || !this.style)
-        return children
+      if (!children || !Array.isArray(children) && !children.props || !this.style) return children
+
+      const styler = this.styleOne.bind(this)
+      if (Array.isArray(children)) return children.map(styler)
 
       const count = React.Children.count(children)
-      const styler = this.styleOne.bind(this)
-
-      if (Array.isArray(children))
-        return children.map(styler)
-      if (count > 1)
-        return React.Children.map(children, styler)
-      else
-        return styler(children)
+      if (count > 1) return React.Children.map(children, styler)
+      return styler(children)
     }
 
     styleOne(child) {
-      if (Array.isArray(child))
-        return this.styleAll(child)
-
-      if (!child || !React.isValidElement(child))
-        return child
+      if (Array.isArray(child)) return this.styleAll(child)
+      if (!child || !React.isValidElement(child)) return child
 
       // <View /> + <tag /> keys
       const name = child.type && (child.type.name || child.type)
 
       // <name $tag /> keys
       const tagged = Object.keys(child.props)
-        .filter(key => key[0] == '$' && child.props[key] === true) // only $props
+        .filter(key => key[0] === '$' && child.props[key] === true) // only $props
         .map(key => key.slice(1)) // remove $
 
       // styles
@@ -58,15 +52,9 @@ export default function Style(ComposedComponent) {
         .map(i => this.stylesheet[i])
         .reduce((acc, cur) => acc.concat(cur || []), [])
 
-      let cloneProps = {}
-
-      if (styles.length)
-        cloneProps.className = css(...styles)
-
-      if (child.props) {
-        if (child.props.children)
-          cloneProps.children = this.styleAll(child.props.children)
-      }
+      const cloneProps = {}
+      if (styles.length) cloneProps.className = css(...styles)
+      if (child.props && child.props.children) cloneProps.children = this.styleAll(child.props.children)
 
       return React.cloneElement(child, cloneProps)
     }
