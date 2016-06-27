6eb6ec76d43e514393b760c2bcdca425591e68ce
steelbrain <de.steelbrain@gmail.com>
2016-06-27 15:05:50 -0700

    :new: Rewrite motion-nice-styles

--- packages/nice-styles/src/index.js
+++ packages/nice-styles/src/index.js
@@ -1,116 +1,68 @@
-import objectToColor from './objectToColor'
+/* @flow */
 
-const isNumerical = (obj, key) =>
-  ['x','y','z'].indexOf(key) >= 0 && typeof obj[key] == 'number'
+import { objectToColor } from './helpers'
+import type { Color, Transform } from './types'
 
-const pseudoAbbrs = {
-  active: ':active',
-  hover: ':hover',
-  focus: ':focus',
-  link: ':link',
-  visited: ':visited',
-  checked: ':checked',
-  disabled: ':disabled',
-  empty: ':empty',
-  invalid: ':invalid',
-}
-
-const pseudos = {
-  ':active': true,
-  ':hover': true,
-  ':focus': true,
-  ':link': true,
-  ':visited': true,
-  ':checked': true,
-  ':disabled': true,
-  ':empty': true,
-  ':invalid': true
-}
-
-const transformKeysMap = {
+const PSEUDO = new Set(['active', 'hover', 'focus', 'link', 'visited', 'checked', 'disabled', 'empty', 'invalid'])
+const COLOR_KEYS = new Set(['background'])
+const TRANSFORM_KEYS_MAP = {
   x: 'translateX',
   y: 'translateY',
   z: 'translateZ'
 }
 
-const arrayToString = val =>
-  val.map(style =>
-    typeof style == 'number' ? `${style}px` : style
-  ).join(' ')
-
-// transformers
-
-function transformColor(styles, attr) {
-  return typeof styles[attr] == 'object'
-    ? objectToColor(styles[attr])
-    : styles[attr]
+function processColor(color: Color): string {
+  return objectToColor(color)
 }
 
-function transformKey(styles, key) {
-  // convert pseudoAbbrs 'active' => ':active'
-  if (pseudoAbbrs[key] && typeof styles[key] == 'object') {
-    styles[pseudoAbbrs[key]] = styles[key]
-    // normalize to :active
-    key = pseudoAbbrs[key]
+function processTransform(transform: Transform): string {
+  const toReturn = []
+  for (const key in transform) {
+    if (!transform.hasOwnProperty(key)) {
+      continue
+    }
+    let value = transform[key]
+    value = typeof value === 'number' ? `${value}px` : value
+    toReturn.push(`${TRANSFORM_KEYS_MAP[key] || key}(${value})`)
   }
+  return toReturn.join(' ')
+}
 
-  // when live coding we type "rc" then "rc()", but "rc" is a function and it breaks, so clear it
-  if (typeof styles[key] == 'function') {
-    styles[key] = null
-    throw new Error(`Passed a function to a style property ${key}`)
+function processArray(array: Array<number | string>): string {
+  return array.map(function(style) {
+    return typeof style === 'number' ? `${style}px` : style
+  })
+}
+
+function processStyles(styles: Object): Object {
+  const toReturn = {}
+  for (const key in styles) {
+    if (!styles.hasOwnProperty(key)) {
+      continue
+    }
+    const value = styles[key]
+    if (typeof value === 'string') {
+      toReturn[key] = value
+      continue
+    }
+    if (COLOR_KEYS.has(key) || key.toLowerCase().indexOf('color')) {
+      toReturn[key] = processColor(value)
+      continue
+    }
+    if (key === 'transform') {
+      toReturn[key] = processTransform(value)
+      continue
+    }
+    if (PSEUDO.has(key) || key.substr(0, 1) === '@') {
+      toReturn[`:${key}`] = processStyles(value)
+      continue
+    }
+    if (Array.isArray(value)) {
+      toReturn[key] = processArray(value)
+    }
+    throw new Error(`Invalid style value for ${key}`)
   }
-
-  // recurse into psuedos
-  if (pseudos[key]) {
-    styleValuesToString(styles[key])
-  }
-
-  // array to string transforms
-  // @media queries
-  if (key[0] == '@')
-    Object.keys(styles[key]).forEach(subKey => { // TODO: make this recursive and use transformKey itself
-      if (Array.isArray(styles[key][subKey]))
-        styles[key][subKey] = arrayToString(styles[key][subKey])
-    })
-  // regular
-  else if (Array.isArray(styles[key]))
-    styles[key] = arrayToString(styles[key])
+  return toReturn
 }
 
-// { transform: { x: 10, y: 10, z: 10 } }
-function transformTransform(styles) {
-  if (typeof styles.transform !== 'object')
-    return styles.transform
-
-  return Object.keys(styles.transform).map(key =>
-    `${transformKeysMap[key] || key}(${styles.transform[key]}${isNumerical(styles.transform, key) ? 'px' : ''})`
-  ).join(' ')
-}
-
-// mutative :(
-export default function styleValuesToString(styles) {
-  if (!styles) return styles
-
-  if (styles.background)
-    styles.background = transformColor(styles, 'background')
-
-  if (styles.backgroundColor)
-    styles.backgroundColor = transformColor(styles, 'backgroundColor')
-
-  if (styles.color)
-    styles.color = transformColor(styles, 'color')
-
-  if (styles.borderColor)
-    styles.borderColor = transformColor(styles, 'borderColor')
-
-  if (styles.transform)
-    styles.transform = transformTransform(styles)
-
-  // final styles
-  Object.keys(styles).forEach(key => transformKey(styles, key))
-}
-
-styleValuesToString.object = (styles) => {
-  styleValuesToString(styles)
-  return styles
-}
+module.exports = processStyles
