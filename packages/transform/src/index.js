1d115808aed71bf59758e75fccab36baba172826
steelbrain <de.steelbrain@gmail.com>
2016-06-27 13:38:06 -0700

    :fire: Remove motion-transform

--- packages/transform/src/index.js
+++
@@ -1,6 +0,0 @@
-import _app from './app'
-import _file from './file'
-
-export const app = _app
-export const file = _file
-export default { app: _app, file: _file }
--- packages/transform/src/nodes/ImportDeclaration.js
+++
@@ -1,5 +0,0 @@
-import state from '../state'
-
-export default (node) => {
-  state.file.imports.push(node.source.value)
-}
