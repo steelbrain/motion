48a14ce7db44b75df7b7a9db056c3070ffca3602
natew <natewienert@gmail.com>
2016-07-14 15:56:51 -0700

    :new: add themes, test themes and motion-style

---
+++ packages/style/spec/style-spec.js
@@ -0,0 +1,79 @@
+'use babel'
+
+/* @flow */
+
+import styler from '../lib/index'
+import { it } from 'jasmine-fix'
+import React from 'react'
+import { mount } from 'enzyme'
+
+const style = styler()
+
+@style
+class StyledComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <h2>Hello</h2>
+      </div>
+    )
+  }
+
+  style = {
+    h1: {
+      background: 'red'
+    },
+    h2: {
+      transform: {
+        x: 0
+      },
+      border: [1, 'solid', '#ccc']
+    },
+    theme: {
+      black: {
+        h1: {
+          background: 'black'
+        }
+      }
+    }
+  }
+}
+
+describe('MotionStyle', () => {
+  it('applies styles', () => {
+    const el = mount(<StyledComponent />)
+
+    const h1 = el.find('h1')
+    const instance = el.component.getInstance()
+
+    // has classname
+    expect(!!h1.props().className).toBe(true)
+    // matches stylesheet
+    expect(h1.props().className).toBe(instance.stylesheet.h1._name)
+    // applies style
+    expect(instance.stylesheet.h1._definition.background).toBe('red')
+  })
+
+  it('applies complex styles', () => {
+    const el = mount(<StyledComponent />)
+
+    const h2 = el.find('h2')
+    const instance = el.component.getInstance()
+
+    // applies style
+    expect(instance.stylesheet.h2._definition.transform).toBe('translateX(0px)')
+    expect(instance.stylesheet.h2._definition.border).toBe('1px solid #ccc')
+  })
+
+  it('applies themes', () => {
+    const el = mount(<StyledComponent theme="black" />)
+
+    const h1 = el.find('h1')
+    const instance = el.component.getInstance()
+
+    // applies style
+    // TODO test this better so its checking actual className match
+    expect(instance.stylesheet['black-h1']._definition.background).toBe('black')
+  })
+})
--- packages/style/src/index.js
+++ packages/style/src/index.js
@@ -2,125 +2,126 @@ import React from 'react'
 import niceStyles from 'motion-nice-styles'
 import { StyleSheet, css } from 'aphrodite'
 
-let opts = {
+export default (opts = {
   theme: true,
-}
+  themeKey: 'theme'
+}) => {
+  return ComposedComponent => {
+    return class StyledComponent extends ComposedComponent {
+      static displayName = ComposedComponent.displayName || ComposedComponent.name
 
-function Style(ComposedComponent) {
-  return class StyledComponent extends ComposedComponent {
-    static displayName = ComposedComponent.displayName || ComposedComponent.name
+      constructor() {
+        super(...arguments)
 
-    constructor() {
-      super(...arguments)
+        if (this.style) {
+          let styles = Object.assign({}, this.style)
 
-      if (this.style) {
-        let styles = Object.assign({}, this.style)
+          // merge theme onto style
+          // given this.props.theme == 'dark' and this.style == { dark: { h1: { color: 'red' } } }
+          // flatten it to: { dark-button: { color: 'black' } }
+          // why? because StyleSheet.create takes a flat object
+          // and `-` not allowed in jsx tags/props so safe to use
+          const propTheme = this.props[opts.themeKey]
 
-        // merge theme onto style
-        // given this.props.theme == 'dark' and this.style == { dark: { h1: { color: 'red' } } }
-        // flatten it to: { dark-button: { color: 'black' } }
-        // why? because StyleSheet.create takes a flat object
-        // and `-` not allowed in jsx tags/props so safe to use
-        const themeKey = this.props.theme
-        if (opts.theme && themeKey) {
-          const themeStyles = styles[themeKey]
+          if (opts.theme && propTheme) {
+            const themeStyles = styles.theme[propTheme]
 
-          if (typeof themeStyles === 'object') {
-            styles = {
-              ...styles,
-              ...Object.keys(themeStyles)
-                  .reduce((res, key) => ({ ...res, [`${themeKey}-${key}`]: themeStyles[key] }), {})
+            console.log(JSON.stringify(styles))
+
+            if (typeof themeStyles === 'object') {
+              styles = {
+                ...styles,
+                // flatten themes to `theme-tag: {}`
+                ...Object.keys(themeStyles)
+                    .reduce((res, key) => ({ ...res, [`${propTheme}-${key}`]: themeStyles[key] }), {})
+              }
+
+              // remove theme key
+              delete styles.theme
+            } else {
+              console.log(`Note: you have a matching key for your theme prop ${propTheme}, but it isn't an object`)
             }
           }
-          else {
-            console.log(`Note: you have a matching key for your theme prop ${themeKey}, but it isn't an object`)
+
+          // nice style them
+          for (const style in styles) {
+            if (!styles.hasOwnProperty(style) || style === opts.themeKey) {
+              continue
+            }
+            const value = styles[style]
+            if (value) {
+              styles[style] = niceStyles(value)
+            }
           }
-        }
 
-        // nice style them
-        for (const style in styles) {
-          if (!styles.hasOwnProperty(style)) {
-            continue
-          }
-          const value = styles[style]
-          if (value) {
-            styles[style] = niceStyles(value)
-          }
-        }
-
-        this.stylesheet = StyleSheet.create(styles)
-      }
-    }
-
-    render() {
-      return this.stylesheet ?
-        this.styleAll.call(this, super.render()) :
-        super.render()
-    }
-
-    styleAll(children) {
-      if (!children || !Array.isArray(children) && !children.props || !this.style) return children
-
-      const styler = this.styleOne.bind(this)
-      if (Array.isArray(children)) {
-        return children.map(styler)
-      }
-
-      const count = React.Children.count(children)
-      if (count > 1) {
-        return React.Children.map(children, styler)
-      }
-
-      return styler(children)
-    }
-
-    styleOne(child) {
-      if (Array.isArray(child)) return this.styleAll(child)
-      if (!child || !React.isValidElement(child)) return child
-
-      // <View /> + <tag /> keys
-      const name = child.type && (child.type.name || child.type)
-
-      // <name $tag /> keys
-      const tagged = Object.keys(child.props)
-        .filter(key => key[0] === '$' && child.props[key] === true) // only $props
-        .map(key => key.slice(1)) // remove $
-
-      const styleKeys = [name, ...tagged]
-
-      // styles
-      let styles = styleKeys
-        .map(i => this.stylesheet[i])
-        .reduce((acc, cur) => acc.concat(cur || []), [])
-
-      // theme styles
-      if (opts.theme && this.props.theme) {
-        styles = [...styles, ...styleKeys.map(k => this.stylesheet[`${this.props.theme}-${k}`])]
-      }
-
-      const cloneProps = {}
-
-      if (styles.length) {
-        cloneProps.className = css(...styles)
-
-        if (child.props && child.props.className) {
-          if (typeof child.props.className === 'string') {
-            cloneProps.className += ` ${child.props.className}`
-          }
+          this.stylesheet = StyleSheet.create(styles)
         }
       }
 
-      if (child.props && child.props.children) {
-        cloneProps.children = this.styleAll(child.props.children)
+      render() {
+        return this.stylesheet ?
+          this.styleAll.call(this, super.render()) :
+          super.render()
       }
 
-      return React.cloneElement(child, cloneProps)
+      styleAll(children) {
+        if (!children || !Array.isArray(children) && !children.props || !this.style) return children
+
+        const styler = this.styleOne.bind(this)
+        if (Array.isArray(children)) {
+          return children.map(styler)
+        }
+
+        const count = React.Children.count(children)
+        if (count > 1) {
+          return React.Children.map(children, styler)
+        }
+
+        return styler(children)
+      }
+
+      styleOne(child) {
+        if (Array.isArray(child)) return this.styleAll(child)
+        if (!child || !React.isValidElement(child)) return child
+
+        // <View /> + <tag /> keys
+        const name = child.type && (child.type.name || child.type)
+
+        // <name $tag /> keys
+        const tagged = Object.keys(child.props)
+          .filter(key => key[0] === '$' && child.props[key] === true) // only $props
+          .map(key => key.slice(1)) // remove $
+
+        const styleKeys = [name, ...tagged]
+
+        // styles
+        let styles = styleKeys
+          .map(i => this.stylesheet[i])
+          .reduce((acc, cur) => acc.concat(cur || []), [])
+
+        // theme styles
+        if (opts.theme && this.props[opts.themeKey]) {
+          styles = [...styles, ...styleKeys.map(k => this.stylesheet[`${this.props[opts.themeKey]}-${k}`])]
+        }
+
+        const cloneProps = {}
+
+        if (styles.length) {
+          cloneProps.className = css(...styles)
+
+          if (child.props && child.props.className) {
+            if (typeof child.props.className === 'string') {
+              cloneProps.className += ` ${child.props.className}`
+            }
+          }
+        }
+
+        if (child.props && child.props.children) {
+          cloneProps.children = this.styleAll(child.props.children)
+        }
+
+        return React.cloneElement(child, cloneProps)
+      }
     }
   }
 }
-
-Style.configure = _opts => {
-  opts = _opts
-}
-
-export default Style
