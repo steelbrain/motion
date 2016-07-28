f393a6d342667ebba4fc6d3ca5e02724cf31bba5
steelbrain <de.steelbrain@gmail.com>
2016-07-28 04:12:59 -0700

    :new: Give users full control over babel config

--- packages/motion/src/types.js
+++ packages/motion/src/types.js
@@ -1,7 +1,12 @@
 /* @flow */
 
 export type Config = {
+  babel: {
+    plugins: [],
+    presets: []
+  },
   webServerPort: number,
+  saveNpmModules: boolean,
   bundleDirectory: string,
   publicDirectory: string,
   includePolyfills: boolean,
