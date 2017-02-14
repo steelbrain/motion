caf884730ca9116bd824e8d359770b7090e0b18f
Steel Brain <i@steelbrain.me>
2017-02-14 23:57:51 +0500

    :new: Trigger dispose before exit

--- packages/motion/src/bin/helpers.js
+++ packages/motion/src/bin/helpers.js
@@ -3,7 +3,20 @@
 import Motion from '../'
 
 export function getMotion(rootDirectory: string = process.cwd(), callback: ((motion: Motion) => any)) {
-  return Motion.create(rootDirectory).then(callback).catch(function(error) {
+  return Motion.create(rootDirectory).then(function(motion) {
+    let motionIsAlive = true
+    function killMotion() {
+      if (motionIsAlive) {
+        motionIsAlive = false
+        motion.dispose()
+      }
+      process.exit()
+    }
+
+    process.on('SIGINT', killMotion)
+    process.on('exit', killMotion)
+    return callback(motion)
+  }).catch(function(error) {
     console.error('Error:', error && (error.motion ? error.message : error.stack))
     process.exitCode = 1
   })
